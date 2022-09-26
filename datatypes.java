ea3f5bd472fc47b39189cb7f4f3f888af877527c
Muhammet Alkan <93428614+xulame@users.noreply.github.com>
2022-09-26 12:45:31 +0300

    Create datatypes.java

---
+++ datatypes.java
@@ -0,0 +1,14 @@
+package javademos;
+
+public class datatypes {
+
+	public static void main(String[] args) {
+		int num = 12;
+		num = 13;
+		char character = 'M';
+		String city = "Manisa";
+		boolean check = false;
+		
+	}
+
+}
