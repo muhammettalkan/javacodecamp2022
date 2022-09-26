6b95712fc72031d1a20f378d8a3361f42cba9542
Muhammet Alkan <93428614+xulame@users.noreply.github.com>
2022-09-26 12:46:06 +0300

    Create variables.java

---
+++ variables.java
@@ -0,0 +1,11 @@
+package javademos;
+
+public class variables {
+
+	public static void main(String[] args) {
+		String text = "Number of Students";
+		System.out.println("Number of Students");
+
+	}
+
+}
