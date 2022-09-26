28e6ee0309f4e7a5ba66b69d5a8495d656fa4168
Muhammet Alkan <93428614+xulame@users.noreply.github.com>
2022-09-26 12:46:50 +0300

    Create conditionals.java

---
+++ conditionals.java
@@ -0,0 +1,16 @@
+package javademos;
+
+public class conditionals {
+
+	public static void main(String[] args) {
+		int number = 21;
+		if (number<20) {
+			System.out.println("Number is smaller than 20");
+		}else if(number == 20) {
+			System.out.println("Number is equal to 20");
+		}else {
+			System.out.println("Number is bigger than 20");
+		}
+	}
+
+}
